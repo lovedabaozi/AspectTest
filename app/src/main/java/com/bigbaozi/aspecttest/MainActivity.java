b7aae0d4970d616137b936b8d0353376bf546d69
fenglin.zhang <fenglin.zhang@haitaichina.com>
2018-05-21 17:37:19 +0800

    添加接口回调演示

--- app/src/main/java/com/bigbaozi/aspecttest/MainActivity.java
+++ app/src/main/java/com/bigbaozi/aspecttest/MainActivity.java
@@ -7,8 +7,9 @@ import android.util.Log;
 import android.view.View;
 
 import com.bigbaozi.aspectjlibrary.annotation.CountTime;
+import com.bigbaozi.aspectjlibrary.interf.ICountTime;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ICountTime {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,22 +20,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @CountTime("Hello")
-    public  void  Hello(){
+    public void Hello() {
 
         SystemClock.sleep(1000);
-        int x=0;
+        int x = 0;
         x++;
     }
 
 
-   @CountTime("Around")
-    public  void  Around(){
+    @CountTime("Around")
+    public void Around() {
 
-       SystemClock.sleep(500);
+        SystemClock.sleep(500);
     }
 
     public void go(View view) {
         Hello();
         Around();
+    }
+
+    /**
+     *  接口回调方法名和时间，可自定义处理记时逻辑。
+     * @param methodName
+     * @param times
+     */
+    @Override
+    public void GetTimes(String methodName, long times) {
+        Log.e("AAA","---"+methodName+"---"+times);
     }
 }
